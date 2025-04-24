b868e8664c2d88a7c050ff4b0364beec40a1a997
DKing0518 <kingdmiyal@gmail.com>
2025-04-24 17:40:36 -0400

    Navigation Bar commits

--- app/src/main/java/com/example/buzzboardfinalproject/MainActivity.kt
+++ app/src/main/java/com/example/buzzboardfinalproject/MainActivity.kt
@@ -7,14 +7,43 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.favorites -> replaceFragment(FavoritesFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+                R.id.add_post-> replaceFragment(AddPostFragment())
+                R.id.messages -> replaceFragment(MessagesFragment())
+
+                else ->{
+
+                }
+            }
+            true
+        }
     }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
